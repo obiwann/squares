f784478a2f7801719d7b7c1ba37a12fb440ed03f
Kristian Remšak <kristianr@Kristians-MacBook-Air.local>
2023-01-22 21:36:39 +0100

    add logic and basic tests - passing

--- src/square-finder.js
+++ src/square-finder.js
@@ -1,9 +1,32 @@
 
+const width = 70;
+const height = 80;
+
 let findSquares = (width, height)=> {
     let squares = [];
+    let x = 0;
+    let y = 0;
 
-    let squareSize = width > height? height: width;
+    while(width > 0) {
+        if(width >= height) {
+            // take height as square size
+                squares.push({x, y, 'width': height, 'height': height})
+                width -= height;
+                x += height;
+            } else {
+            // take width as sqaure size
+                squares.push({x, y, 'width': width, 'height': width})
+                height -= width;
+                y += width;
+            }
+    }
+
+    //console.log(squareSize);
     return squares;
 }
+
+let squares = findSquares(width, height);
+console.log("squares:");
+console.log(squares);
 
 export default findSquares;
--- test/square-finder.test.js
+++ test/square-finder.test.js
@@ -5,9 +5,28 @@ console.log("testing!!");
 
 describe('findSquares', () => {
     it('should return squares', () => {
-        let squares = findSquares(5,5);
+        let squares = findSquares(20,20);
         assert.deepEqual(squares, [
-            {x: 0, y: 0, width: 5, height: 5},
+            {x: 0, y: 0, width: 20, height: 20},
+        ])
+        squares = findSquares(20,40);
+        assert.deepEqual(squares, [
+            {x: 0, y: 0, width: 20, height: 20},
+            {x: 0, y:20, width: 20, height: 20},
+        ])
+        squares = findSquares(40,20);
+        assert.deepEqual(squares, [
+            {x: 0, y: 0, width: 20, height: 20},
+            {x: 20, y: 0, width: 20, height: 20},
+        ])
+        squares = findSquares(14,63);
+        assert.deepEqual(squares, [
+            {x: 0, y: 0, width: 14, height: 14},
+            {x: 0, y: 14, width: 14, height: 14},
+            {x: 0, y: 28, width: 14, height: 14},
+            {x: 0, y: 42, width: 14, height: 14},
+            {x: 0, y: 56, width: 7, height: 7},
+            {x: 7, y: 56, width: 7, height: 7},
         ])
     }) 
 });
